78b623b2c8183297882371e36a153f29c55da4f2
zvxayr <carlo4noles@gmail.com>
2018-03-11 21:42:41 +0800

    yay

--- controllers/root.js
+++ controllers/root.js
@@ -1,10 +1,11 @@
-const fs = require('fs-extra')
+const ejs = require('ejs')
+const fs  = require('fs-extra')
 
 let prefix = ''
 
 function route(router) {
     router.get('/', async ctx => {
-        ctx.body = await fs.readFile('./views/home.html', 'utf8')
+    	ctx.body = await ctx.render('dashboard')
     })
 
     return router
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const serve          = require('koa-static')
 const logger         = require('koa-logger')
 const bodyparser     = require('koa-bodyparser')
 const methodoverride = require('koa-methodoverride')
+const fs             = require('fs-extra')
 const http           = require('http')
+const ejs            = require('ejs')
 
 const app    = new Koa()
 const server = http.createServer(app.callback())
@@ -13,6 +15,12 @@ const controller  = require('./controllers')
 const api         = require('./api')
 
 require('./socketio')(server)
+
+app.context.render = async (path, data, options) => {
+	let template = await fs.readFile(`./views/${path}.ejs`, 'utf8')
+
+	return ejs.render(template, data, options)
+}
 
 app.use(methodoverride())
 app.use(logger())
